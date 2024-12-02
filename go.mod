755eb1f4b5306a17644540e570f3fc12d8ba6be7
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-12-02 18:54:21 +0700

    Add struct

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Proyek-Three/be-promosi-umkm
+
+go 1.23.3
+
+require go.mongodb.org/mongo-driver v1.17.1
--- model/type.go
+++ model/type.go
@@ -6,28 +6,36 @@ import (
 
 // Users represents the table Users
 type Users struct {
-	ID       int    `json:"id" gorm:"primaryKey;autoIncrement"`
-	Nama     string `json:"nama"`
-	Role     string `json:"role"`
-	Username string `json:"username" gorm:"unique;not null"`
-	Password string `json:"password"`
-	NoTelp   string `json:"no_telp"`
-	Alamat   string `json:"alamat"`
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"id,omitempty"`
+	Name        string             `json:"name,omitempty" bson:"name,omitempty"`
+	Role        string             `json:"role,omitempty" bson:"role,omitempty"`
+	Username    string             `json:"username,omitempty" bson:"username,omitempty" gorm:"unique;not null"`
+	Password    string             `json:"password,omitempty" bson:"password,omitempty"`
+	PhoneNumber string             `json:"phone_number,omitempty" bson:"phone_number,omitempty"`
+	Email       string             `json:"email,omitempty" bson:"email,omitempty"`
 }
 
 // Product represents the table Produk
 type Product struct {
-	ID         int            `json:"id" gorm:"primaryKey;autoIncrement"`
-	NamaProduk string         `json:"nama_produk"`
-	NamaToko   string         `json:"nama_toko"`
-	Deskripsi  string         `json:"deskripsi"`
-	Gambar     string         `json:"gambar"`
-	Harga      int            `json:"harga"`
-	Category   NamaCategory   `bson:"nama_category,omitempty" json:"nama_category,omitempty"`
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	ProductName  string             `json:"product_name,omitempty" bson:"product_name,omitempty"`
+	Name         string             `json:"nama_toko,omitempty" bson:"nama_toko,omitempty"`
+	Description  string             `json:"deskripsi,omitempty" bson:"deskripsi,omitempty"`
+	Image        string             `json:"gambar,omitempty" bson:"gambar,omitempty"`
+	Price        int                `json:"harga,omitempty" bson:"harga,omitempty"`
+	CategoryName Category           `bson:"category_name,omitempty" json:"category_name,omitempty"`
+	StoreName    Store              `bson:"store_name,omitempty" json:"store_name,omitempty"`
 }
 
-// Category represents the table Categori
+// Store represents the table Store
+type Store struct {
+	ID        int    `json:"_id,omitempty" bson:"id,omitempty"`
+	StoreName string `json:"store_name,omitempty" bson:"store_name,omitempty"`
+	Address   string `json:"address,omitempty" bson:"address,omitempty"`
+}
+
+// Category represents the table Category
 type Category struct {
-	ID           int    `json:"id" gorm:"primaryKey;autoIncrement"`
-	NamaCategory string `json:"nama_category"`
+	ID           int    `json:"id,omitempty" bson:"id,omitempty" gorm:"primaryKey;autoIncrement"`
+	CategoryName string `json:"category_name,omitempty" bson:"category_name,omitempty"`
 }
