6466a5244a07243487fec3e4f0d9c097d2c3e6c0
jul003 <dzulkiflifaiz11@gmail.com>
2024-12-31 22:55:50 +0700

    fix error in admin.go

--- module/admin.go
+++ module/admin.go
@@ -2,16 +2,11 @@ package module
 
 import (
     "github.com/gofiber/fiber/v2"
-    "github.com/gofiber/fiber/v2/middleware/logger"
-    "github.com/Proyek-Three/bp-promosi-umkm/controller" 
+    "github.com/gofiber/fiber/v2/middleware/logger" 
 )
 
 func main() {
     r := fiber.New()
     r.Use(logger.New())
-
-    // Route login
-    r.Post("/login", controller.LoginAdmin)
-
     r.Listen(":8080")
 }
