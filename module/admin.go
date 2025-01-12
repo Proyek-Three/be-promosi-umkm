c729a2360d379364ee2679391fc5efe2aa4973db
jul003 <dzulkiflifaiz11@gmail.com>
2025-01-12 22:19:49 +0700

    back to admin credentials

--- module/admin.go
+++ module/admin.go
@@ -1,35 +1,14 @@
 package module
 
 import (
-    "time"
-    "github.com/golang-jwt/jwt/v5"
+    "github.com/Proyek-Three/be-promosi-umkm/model"
 )
 
-var jwtKey = []byte("jdsfsdfsd54353454354235234") 
-
-// GenerateToken membuat token JWT
-func GenerateToken(username string) (string, error) {
-    claims := &jwt.RegisteredClaims{
-        Subject:   username,
-        ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)), 
-    }
-
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-    return token.SignedString(jwtKey)
+var AdminCredentials = model.Admin{
+	Username: "admin",
+	Password: "admin123",
 }
 
-// ValidateToken memvalidasi token JWT
-func ValidateToken(tokenString string) (*jwt.RegisteredClaims, error) {
-    token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
-        return jwtKey, nil
-    })
-    if err != nil {
-        return nil, err
-    }
-
-    if claims, ok := token.Claims.(*jwt.RegisteredClaims); ok && token.Valid {
-        return claims, nil
-    }
-
-    return nil, err
+func ValidateAdmin(input model.Admin) bool {
+	return input.Username == AdminCredentials.Username && input.Password == AdminCredentials.Password
 }
